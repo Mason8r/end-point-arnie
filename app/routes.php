3a51afb081ae00fd36eba86f2761681c40d75441
Stu Mason <stuart.a.mason@gmail.com>
2015-12-19 18:15:14 +0000

    Laravel!

---
+++ app/routes.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::match(array('GET', 'POST'),'/', function()
+{
+	if(Input::has('inspect')) {
+
+		$tests = file_get_contents('end-point-arnie');
+		$array = array_reverse(explode(PHP_EOL,$tests));
+		foreach ($array as $request) {
+			if(strlen($request) > 0) {
+				$data = unserialize($request);
+				foreach ($data as $key => $value) {
+					if($key == 'time' ) {
+						$value = date('l d F g.ia',$value);
+					}
+
+					if(is_array($value) || is_object($value)) {
+						var_dump($value);
+					} else {
+						echo "<strong>".$key .":</strong> ".$value."<br />";
+					}
+				}
+			}
+		}
+
+	} else {
+
+		if($_SERVER['CONTENT_TYPE'] != 'application/json') {
+			if($_POST) {
+				$body_content = $_POST;
+			} else {
+				$body_content = file_get_contents('php://input');
+			}
+		} else {
+			$body_content = json_decode(file_get_contents('php://input'));
+		}
+
+		$request = [
+			'method' => $_SERVER['REQUEST_METHOD'],
+			'content_type' => $_SERVER['CONTENT_TYPE'],
+			'content_length' => $_SERVER['CONTENT_LENGTH'],
+			'request_uri' => $_SERVER['REQUEST_URI'],
+			'protocol' => $_SERVER['SERVER_PROTOCOL'],
+			'remote_address' => $_SERVER['REMOTE_ADDR'],
+			'remote_port' => $_SERVER['REMOTE_PORT'],
+			'user_agent' => $_SERVER['HTTP_USER_AGENT'],
+			'time' => $_SERVER['REQUEST_TIME'],
+			'body' => $body_content,
+		];
+		$store_payload = file_put_contents('end-point-arnie', serialize($request).PHP_EOL, FILE_APPEND);
+		if($store_payload) {
+			echo 'success';
+		}	
+	}
+
+});
--- public/index.php
+++ public/index.php
@@ -1,55 +1,49 @@
-<?php 
+<?php
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
 
-if(isset($_GET['inspect'])) {
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
 
-	$tests = file_get_contents('end-point-arnie');
-	$array = array_reverse(explode(PHP_EOL,$tests));
-	foreach ($array as $request) {
-		if(strlen($request) > 0) {
-			$data = unserialize($request);
-			foreach ($data as $key => $value) {
-				if($key == 'time' ) {
-					$value = date('l d F g.ia',$value);
-				}
+require __DIR__.'/../bootstrap/autoload.php';
 
-				if(is_array($value) || is_object($value)) {
-					var_dump($value);
-				} else {
-					echo "<strong>".$key .":</strong> ".$value."<br />";
-				}
-			}
-		}
-	}
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let's turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight these users.
+|
+*/
 
-} else {
+$app = require_once __DIR__.'/../bootstrap/start.php';
 
-	if($_SERVER['CONTENT_TYPE'] != 'application/json') {
-		if($_POST) {
-			$body_content = $_POST;
-		} else {
-			$body_content = file_get_contents('php://input');
-		}
-	} else {
-		$body_content = json_decode(file_get_contents('php://input'));
-	}
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can simply call the run method,
+| which will execute the request and send the response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have whipped up for them.
+|
+*/
 
-	$request = [
-		'method' => $_SERVER['REQUEST_METHOD'],
-		'content_type' => $_SERVER['CONTENT_TYPE'],
-		'content_length' => $_SERVER['CONTENT_LENGTH'],
-		'request_uri' => $_SERVER['REQUEST_URI'],
-		'protocol' => $_SERVER['SERVER_PROTOCOL'],
-		'remote_address' => $_SERVER['REMOTE_ADDR'],
-		'remote_port' => $_SERVER['REMOTE_PORT'],
-		'user_agent' => $_SERVER['HTTP_USER_AGENT'],
-		'time' => $_SERVER['REQUEST_TIME'],
-		'body' => $body_content,
-	];
-	$store_payload = file_put_contents('end-point-arnie', serialize($request).PHP_EOL, FILE_APPEND);
-	if($store_payload) {
-		echo 'success';
-	}	
-}
-	
-
-
+$app->run();
